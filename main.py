0894e84e0527df179fbd8c0eb390fe904317865e
Justin Cueva <justincueva@Justins-MBP.lan>
2022-07-06 22:30:11 -0700

    ok cancel popup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import messagebox
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
@@ -9,10 +10,15 @@ def save():
     email = email_entry.get()
     password = password_entry.get()
 
-    with open('data.txt', 'a') as data_file:
-        data_file.write(f'{website} | {email} | {password} \n')
-        website_entry.delete(0, END)
-        password_entry.delete(0, END)
+    is_ok = messagebox.askokcancel(title=website,
+                                   message=f"These are the details entered: \nEmail: {email} \nPassword: "
+                                           f"{password} \nIs it ok to save?")
+
+    if is_ok:
+        with open('data.txt', 'a') as data_file:
+            data_file.write(f'{website} | {email} | {password} \n')
+            website_entry.delete(0, END)
+            password_entry.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
