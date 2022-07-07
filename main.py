08c4a80cba58cd7741ed480555af7679c5d5b2c2
Justin Cueva <justincueva@Justins-MBP.lan>
2022-07-06 17:08:05 -0700

    displaying our buttons

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from tkinter import *
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
@@ -25,10 +25,16 @@ password_label.grid(row=3, column=0)
 
 # Entries
 website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1)
+website_entry.grid(row=1, column=1, columnspan=2)
 email_entry = Entry(width=35)
-email_entry.grid(row=2, column=1)
+email_entry.grid(row=2, column=1, columnspan=2)
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
